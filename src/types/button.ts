a8ed71e32d9780e181554eac929bfd67428bf235
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-04-25 20:39:41 -0300

    adjust

--- src/types/button.ts
+++ src/types/button.ts
@@ -1,6 +1,12 @@
 export interface ButtonProps {
-   onClick: void
+   onClick: any
    disable?: boolean
    variant: "primary" | "secondary" | "danger" | "success"
    children: React.ReactNode
+   isLoading?: boolean
+}
+
+export interface ButtonSectionPros {
+   idProduct: string
+   productName: string
 }
