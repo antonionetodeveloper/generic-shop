a7d67f832be940b04e2e66461d033515fc8b69d7
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-06-01 22:06:18 -0300

    fixes

--- src/app/(pages)/product/[id]/page.tsx
+++ src/app/(pages)/product/[id]/page.tsx
@@ -8,24 +8,6 @@ import Loading from "@/app/loading"
 
 export const revalidate = 60 * 60 // 1 hour
 
-export async function generateStaticParams() {
-   const response = await fetch(
-      `https://api.stripe.com/v1/products?active=true`,
-      {
-         headers: {
-            Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
-         },
-      }
-   )
-   const product = await response.json()
-
-   return product.data.map((product: { id: string }) => ({
-      params: {
-         id: product.id,
-      },
-   }))
-}
-
 export async function generateMetadata(context: Context): Promise<Metadata> {
    const { id } = context.params
 
@@ -36,7 +18,7 @@ export async function generateMetadata(context: Context): Promise<Metadata> {
    })
    const product = await response.json()
 
-   return { title: product.title }
+   return { title: product.name }
 }
 
 export default function Product({ params }: ProductProps) {
